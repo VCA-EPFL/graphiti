3da4604316e1a8c297a2d11e590d27c3cbf54cc6
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2025-11-17 15:58:38 +0100

    Liveness2: fix compHist module

--- Graphiti/Projects/Liveness/ComposedModuleHist.lean
+++ Graphiti/Projects/Liveness/ComposedModuleHist.lean
@@ -6,15 +6,16 @@ Authors: Maï-Linh Cordonnier
 
 import Graphiti.Core.Module
 import Graphiti.Core.AssocList.Basic
+import Graphiti.Core.Trace
 
 namespace Graphiti.NatModule
 
--- g ∘ f. state = (f(in), g(f(in)), trace_history)
-@[drunfold, drcomponents] def gcompf (T : Type)(f g : T -> T) : NatModule ((List T) × (List T) × (List T)) :=
+-- g ∘ f. state = (f(in), g(f(in)), trace_history [new, old])
+@[drunfold, drcomponents] def gcompfHist (T : Type)(f g : T -> T) : NatModule ((List T) × (List T) × (Trace Nat)) :=
   {
-    inputs := [(0, ⟨ T, λ s tt s' => s' = (s.fst.concat (f tt), s.snd.fst, tt :: s.snd.snd) ⟩)].toAssocList,
+    inputs := [(0, ⟨ T, λ s tt s' => s' = (s.fst.concat (f tt), s.snd.fst, (.input 0 ⟨T, tt⟩) :: s.snd.snd) ⟩)].toAssocList,
     internals := [λ s s' => s' = (∅, s.snd.fst ++ s.fst.map g, s.snd.snd)],
-    outputs := [(0, ⟨ T, λ s tt s' => (s.fst, s.snd.fst, tt :: s.snd.snd) = (s'.fst, tt :: s'.snd.fst, s'.snd.snd) ⟩)].toAssocList,
+    outputs := [(0, ⟨ T, λ s tt s' => (s.fst, s.snd.fst, (.output 0 ⟨T, tt⟩) :: s.snd.snd) = (s'.fst, tt :: s'.snd.fst, s'.snd.snd) ⟩)].toAssocList,
     init_state := λ s => s = ([], [], []),
   }
 
