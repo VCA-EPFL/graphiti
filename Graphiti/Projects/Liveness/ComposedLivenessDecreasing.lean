6deb0b47f29b672a64b8e9cec1ff10b8683a0d9e
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2025-11-17 15:59:24 +0100

    Liveness2: add weight func

---
+++ Graphiti/Projects/Liveness/ComposedLivenessDecreasing.lean
@@ -0,0 +1,26 @@
+/-
+Copyright (c) 2025 VCA Lab, EPFL. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Maï-Linh Cordonnier, Lorenzo Padrini
+-/
+
+import Graphiti.Core.ModuleLemmas
+import Graphiti.Core.StateTransition
+import Graphiti.Projects.Liveness.ComposedModuleHist
+import Graphiti.Projects.Liveness.StateTransitionLiveness
+import Graphiti.Core.Trace
+
+
+open Graphiti
+
+-- Calc #inputs - #outputs in history
+def count_in_out (history : List (IOEvent Nat)) : Nat :=
+  history.foldl (fun acc event =>
+    match event with
+    | IOEvent.input _ _ => acc + 1
+    | IOEvent.output _ _ => acc - 1
+  ) 0
+
+def gcompfHistWeight {T} {f g : T -> T} (s : Graphiti.Module.State Nat ((List T) × ((List T) × (Trace Nat)))) : Nat :=
+  let s_stt := s.state;
+  2 * List.length s_stt.fst + List.length s_stt.snd.fst + count_in_out s_stt.snd.snd
