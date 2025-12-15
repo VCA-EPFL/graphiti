cb45751fbdf457dcd227a71b9b4f407504d960ce
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2025-12-15 11:20:55 +0100

    liveness: start modhist correct1

--- Graphiti/Projects/Liveness/ModuleHistory.lean
+++ Graphiti/Projects/Liveness/ModuleHistory.lean
@@ -19,8 +19,43 @@ def generate_history (m : Module Ident S) : Module Ident (Trace Ident × S) :=
     internals := m.internals.map (λ rule => λ s s' => rule s.2 s'.2 ∧ s.1 = s'.1)
   }
 
+theorem generate_history_correct1_star {m : Module Ident S} {t1 t2 : Trace Ident} :
+  ∀ s1 s2,
+  @reachable _ _ (Module.state_transition (generate_history m)) t1 s1
+  → @star _ _ (Module.state_transition (generate_history m)) s1 t2 s2
+  → s1.module = (generate_history m)
+  → s1.state.1 = t1
+  → s2.state.1 = t1++t2
+  := by
+    intros s1 s2 h_reach_s1 h_s1_star_s2 h_mod h_hist_s1
+    induction h_s1_star_s2
+    case refl s3 =>
+      simp; exact h_hist_s1
+    case step s3 s4 s5 t3 t4 h_step h_star ih =>
+
+
+      sorry
+
 theorem generate_history_correct1 {m : Module Ident S} {t : Trace Ident} :
-  ∀ s', @reachable _ _ (Module.state_transition (generate_history m)) t s' → s'.state.1 = t := by sorry
+  ∀ s', @reachable _ _ (Module.state_transition (generate_history m)) t s' → s'.state.1 = t := by
+  intros s_end h_reach
+  have h_reach_end := h_reach
+  obtain ⟨ s_init, ⟨ h_init, h_mod ⟩, h_star ⟩ := h_reach
+  have h_reach_start : @reachable _ _ (Module.state_transition (generate_history m)) [] s_init := by
+    unfold reachable
+    exists s_init
+    apply And.intro
+    . unfold StateTransition.init
+      apply And.intro
+      . assumption
+      . assumption
+    . apply @star.refl _ _ (Module.state_transition (generate_history m))
+
+  simp [drunfold, Module.state_transition] at h_init;
+  have ⟨ h_init_state, h_empty_hist ⟩ := h_init
+  have h_rec := generate_history_correct1_star _ _ h_reach_start h_star h_mod
+  apply h_rec
+  exact h_empty_hist
 
 theorem generate_history_correct2 {m : Module Ident S} {s} {t : Trace Ident} :
   @reachable _ _ (Module.state_transition m) t s →
