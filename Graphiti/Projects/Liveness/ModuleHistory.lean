75db410dd47719bd00f42129f49d748b5a1ae266
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2026-01-06 13:54:37 +0100

    liveness: modHist stuck

--- Graphiti/Projects/Liveness/ModuleHistory.lean
+++ Graphiti/Projects/Liveness/ModuleHistory.lean
@@ -19,6 +19,40 @@ def generate_history (m : Module Ident S) : Module Ident (Trace Ident × S) :=
     internals := m.internals.map (λ rule => λ s s' => rule s.2 s'.2 ∧ s.1 = s'.1)
   }
 
+theorem generate_history_correct1_base {m : Module Ident S} {t : Trace Ident} :
+  ∀ s1 s2, @star _ _ (Module.state_transition (generate_history m)) s1 t s2 → s1.module = (generate_history m) → s2.state.1 = s1.state.1 ++ t := by
+  intros s1 s2 h_star h_mod
+  induction h_star
+  case refl s =>
+    simp
+  case step s1' s2' s3' t1 t2 h_step h_star ih =>
+    rcases s1' with ⟨ ⟨ st1hist, st1 ⟩, mod1 ⟩; rcases s2' with ⟨ ⟨ st2hist, st2 ⟩, mod2 ⟩; rcases s3' with ⟨ ⟨ st3hist, st3 ⟩, mod3 ⟩;
+    simp at *; subst_vars
+    cases h_step --<;> try rw [PortMap.rw_rule_execution (by simp [drunfold]; rfl)] at *
+    case input _ ip a b c d =>
+      -- rw [PortMap.rw_rule_execution (by simp [drunfold, generate_history]; rfl)] at *
+      -- simp [List.getD, Option.map, Option.getD, generate_history, List.find?] at d
+      -- cases d
+      -- simp [generate_history] at d
+
+      subst_vars
+      simp at *
+
+      rw [PortMap.rw_rule_execution (by simp [drunfold, generate_history]; rfl)] at d
+
+      sorry
+
+      -- intros h_mod
+
+    case output => sorry
+    case internal _ r relInt h_step =>
+      simp at *
+      -- rcases s1' with ⟨ ⟨ st1hist, st1 ⟩, mod1 ⟩; rcases new_s with ⟨ stnew, s1hist ⟩ ; simp at *; subst_vars
+      simp [generate_history] at relInt
+      rcases relInt with ⟨ r', relInt', h_rel ⟩
+      rw [← h_rel] at h_step
+      sorry
+
 theorem generate_history_correct1_star {m : Module Ident S} {t1 t2 : Trace Ident} :
   ∀ s1 s2,
   @reachable _ _ (Module.state_transition (generate_history m)) t1 s1
@@ -51,11 +85,22 @@ theorem generate_history_correct1 {m : Module Ident S} {t : Trace Ident} :
       . assumption
     . apply @star.refl _ _ (Module.state_transition (generate_history m))
 
-  simp [drunfold, Module.state_transition] at h_init;
+  simp [ Module.state_transition] at h_init;
   have ⟨ h_init_state, h_empty_hist ⟩ := h_init
-  have h_rec := generate_history_correct1_star _ _ h_reach_start h_star h_mod
-  apply h_rec
-  exact h_empty_hist
+
+  -- have h_rec := generate_history_correct1_star _ _ h_reach_start h_star h_mod
+  -- apply h_rec
+  -- exact h_empty_hist
+
+  -- induction h_star
+  -- . assumption
+  -- case step s1 s2 s3 t1 t2 t3 h_step s_star ih =>
+  --   sorry
+
+  have h_rec := generate_history_correct1_base s_init s_end h_star h_mod
+  rw [h_empty_hist] at h_rec
+  simp at h_rec
+  exact h_rec
 
 theorem generate_history_correct2 {m : Module Ident S} {s} {t : Trace Ident} :
   @reachable _ _ (Module.state_transition m) t s →
