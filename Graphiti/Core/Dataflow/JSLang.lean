dd14ad94a2b349bacb064dfa06fab0ba074b74bb
Yann Herklotz <git@yannherklotz.com>
2026-03-04 23:07:03 +0100

    jslang: use sexpr as intermediate representation

--- Graphiti/Core/Dataflow/JSLang.lean
+++ Graphiti/Core/Dataflow/JSLang.lean
@@ -22,34 +22,65 @@ inductive JSLang where
 | join : String → JSLang → JSLang → JSLang
 | split1 : String → JSLang → JSLang
 | split2 : String → JSLang → JSLang
-| pure : String -> JSLang -> JSLang
+| pure : String → JSLang → JSLang
 | I : JSLang
-deriving DecidableEq, Repr
+deriving DecidableEq, Repr, Inhabited
 
-def toSExpr : JSLang → String
-| .join s l1 l2 =>
-  s!"(join \"{s}\" {toSExpr l1} {toSExpr l2})"
-| .split1 s l =>
-  s!"(split1 \"{s}\" {toSExpr l})"
-| .split2 s l =>
-  s!"(split2 \"{s}\" {toSExpr l})"
-| .pure s l =>
-  s!"(pure \"{s}\" {toSExpr l})"
-| .I => "I"
+inductive SExpr (Atom : Type _) : Type _ where
+| atom : Atom → SExpr Atom
+| list : List (SExpr Atom) → SExpr Atom
+deriving Repr, Inhabited
 
-structure JSLang.Info where
+def SExpr.toString {Atom} [ToString Atom] : SExpr Atom → String
+| .atom a => ToString.toString a
+| .list l => s!"({l.map toString |>.intersperse " " |>.foldl (fun st s => st ++ s) ""})"
+
+instance {Atom} [ToString Atom] : ToString (SExpr Atom) := ⟨SExpr.toString⟩
+
+inductive JSAtom where
+| str (s : String)
+| sym (s : String)
+deriving Repr, Inhabited, DecidableEq
+
+def JSAtom.toString : JSAtom → String
+| .str s => s!"\"{s}\""
+| .sym s => s
+
+instance : ToString JSAtom := ⟨JSAtom.toString⟩
+
+@[simp] abbrev JSSExpr := SExpr JSAtom
+
+def JSLang.toSExpr : JSLang → JSSExpr
+| .join s l1 l2 => .list [.atom (.sym "join"), .atom (.str s), toSExpr l1, toSExpr l2]
+| .split1 s l => .list [.atom (.sym "split1"), .atom (.str s), toSExpr l]
+| .split2 s l => .list [.atom (.sym "split2"), .atom (.str s), toSExpr l]
+| .pure s l => .list [.atom (.sym "pure"), .atom (.str s), toSExpr l]
+| .I => .atom (.sym "I")
+
+structure JSLang.Info Typ where
   inst : String
-  typ : String × Nat
+  typ : Typ
   inPort : String
   outPort : String
 
-instance : Coe (NextNode String (String × Nat)) (JSLang.Info) where
-  coe a := ⟨a.inst, a.typ, a.inputPort, a.outputPort⟩
+def JSLang.Info.ofNextNode {Typ} (a : NextNode String Typ) : JSLang.Info Typ :=
+  ⟨a.inst, a.typ, a.inputPort, a.outputPort⟩
 
-instance : Coe (Std.HashMap String (Array (NextNode String (String × Nat)))) (Std.HashMap String (Array JSLang.Info)) where
-  coe a := a.map (fun _ v => v.map Coe.coe)
+def JSLang.ofNNHashMap {Typ} (a : Std.HashMap String (Array (NextNode String Typ))): Std.HashMap String (Array (JSLang.Info Typ)) :=
+  a.map (fun _ v => v.map JSLang.Info.ofNextNode)
 
-def JSLang.construct (term : Nat) (succ : Std.HashMap String (Array JSLang.Info)) (endN : String) (startN : JSLang.Info) : Option JSLang :=
+class JSType (Typ : Type _) where
+  isSplit : Typ → Bool
+  isJoin : Typ → Bool
+  isPure : Typ → Bool
+
+instance : JSType (String × Nat) where
+  isSplit | ("split", _) => true | _ => false
+  isJoin  | ("join", _) =>  true | _ => false
+  isPure  | ("pure", _) =>  true | _ => false
+
+def JSLang.construct {Typ : Type _} [JSType Typ] (term : Nat)
+    (succ : Std.HashMap String (Array (JSLang.Info Typ))) (endN : String) (startN : JSLang.Info Typ) : Option JSLang :=
   if startN.inst = endN then .some .I else
   match term with
   | 0 => .none
@@ -64,9 +95,9 @@ def JSLang.construct (term : Nat) (succ : Std.HashMap String (Array JSLang.Info)
         return .join startN.inst jsB jsA
     | .some [a] => do -- split node
       let js ← construct term' succ endN a
-      if "pure" == startN.typ.1 then
+      if JSType.isPure startN.typ then
         return pure startN.inst js
-      if "split" == startN.typ.1 && startN.inPort = "out1" then
+      if JSType.isSplit startN.typ && startN.inPort = "out1" then
         return split1 startN.inst js
       else
         return split2 startN.inst js
@@ -131,16 +162,17 @@ def runCommandWithStdin (cmd : String) (args : Array String) (stdin : String) : 
   let stdout ← IO.ofExcept stdout.get
   pure { exitCode := exitCode, stdout := stdout, stderr := stderr }
 
-def rewriteWithEgg {n} (eggCmd := "graphiti_oracle") (p : Pattern String (String × Nat) n) (rewrittenExprHigh : ExprHigh String (String × Nat))
+def rewriteWithEgg {n} (eggCmd := "graphiti_oracle")
+    (p : Pattern String (String × Nat) n) (rewrittenExprHigh : ExprHigh String (String × Nat))
     : IO (List JSLangRewrite) := do
   let .some succ := calcSucc rewrittenExprHigh.invert | throw (.userError s!"{decl_name%}: could not calculate succ")
   let .ok ([first, last], _) := p rewrittenExprHigh
     | return [] -- throw (.userError s!"{decl_name%}: first/last not found")
-  let .some constructed := JSLang.construct 10000 succ first (⟨last, default, default, default⟩)
+  let .some constructed := JSLang.construct 10000 (JSLang.ofNNHashMap succ) first ⟨last, default, default, default⟩
     | throw (.userError s!"{decl_name%}: could not construct")
 
   /- IO.eprintln (toSExpr constructed) -/
-  let out ← runCommandWithStdin eggCmd #[] (toSExpr constructed)
+  let out ← runCommandWithStdin eggCmd #[] (toString constructed.toSExpr)
   IO.ofExcept <| parseRewrites out.stdout
 
 def JSLang.upd1 (m : AssocList String (Option String)) : JSLangRewrite → RewriteResult String (String × Nat) JSLangRewrite
@@ -163,5 +195,12 @@ def JSLang.upd1 (m : AssocList String (Option String)) : JSLangRewrite → Rewri
 
 def JSLang.upd (m : AssocList String (Option String)) (j : List JSLangRewrite) : RewriteResult String (String × Nat) (List JSLangRewrite) :=
   j.mapM (JSLang.upd1 m)
+
+def JSLang.elimPure : JSLang → JSLang
+| .pure s l => l
+| .join s l1 l2 => .join s (elimPure l1) (elimPure l2)
+| .split1 s l => .split1 s (elimPure l)
+| .split2 s l => .split2 s (elimPure l)
+| .I => .I
 
 end Graphiti
